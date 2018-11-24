f81d6b93ece5cf5b25840c8a24eaec7bfb24073b
Le Duc Anh <banhbaomocmeo@gmail.com>
2018-11-24 20:45:25 +0700

    ep 6: Scrolling - 1

---
+++ public/js/Camera.js
@@ -0,0 +1,7 @@
+import {Vec2D} from './math.js'
+
+export default class Camera{
+    constructor() {
+        this.pos = new Vec2D(0, 0)
+    }
+}
--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@ import {createMario} from './entities.js'
 import Timer from './Timer.js'
 import {createCollisionLayer} from './layers.js'
 import {setupKeyboard} from './input.js'
+import Camera from './Camera.js';
 
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
@@ -13,6 +14,8 @@ Promise.all([loadLevel('1-1'), createMario()])
 .then(([level, mario]) => {
 	const FPS = 60
 	
+	const camera = new Camera()
+	window.camera = camera
 	mario.pos.set(64, 180)
 	level.entities.add(mario)
 	level.comp.layers.push(createCollisionLayer(level));
@@ -24,8 +27,9 @@ Promise.all([loadLevel('1-1'), createMario()])
 	// FPS
 	const timer = new Timer(1/FPS)
 	timer.update = function update(deltaTime) {
-		level.comp.draw(context)
 		level.update(deltaTime)
+		level.comp.draw(context, camera)
+
 	}
 	timer.start()
 })
